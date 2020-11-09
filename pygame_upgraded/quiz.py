2b7724442bfbf206f630a14270966de8a5d134c9
Jacob <jacob.nordlund@student.kyh.se>
2020-11-09 10:25:48 +0100

    Changing some quiz and hp stuff

--- pygame_upgraded/quiz.py
+++ pygame_upgraded/quiz.py
@@ -186,17 +186,19 @@ class QuizScreen:
 
 class QuizFinishedScreen:
     def __init__(self, num_of_correct_ans, number_of_quiz_questions):
+        # if num_of_correct_ans >= number_of_quiz_questions - 1:
+        #     info_text = "WOW you're awesome! You get 10 health points as an award! "
+        #     sound("music/kids_cheering.mp3")
+        #     poketer.add_health(10)
+        # else:
+        #     info_text = f"You need at least {number_of_quiz_questions - 1} correct answers to get a bonus. " \
+        #                 f"Better luck next time!"
+        #     sound("music/booing_crowd.mp3")
 
         pygame.mixer.music.stop()
-        if num_of_correct_ans >= number_of_quiz_questions - 1:
-            info_text = "WOW you're awesome! You get 10 health points as an award! "
-            sound("music/kids_cheering.mp3")
-            poketer.add_health(10)
-        else:
-            info_text = f"You need at least {number_of_quiz_questions - 1} correct answers to get a bonus. " \
-                        f"Better luck next time!"
-            sound("music/booing_crowd.mp3")
-
+        percent_correct = num_of_correct_ans / number_of_quiz_questions
+        poketer.add_health(int(50 * percent_correct))
+        info_text = f"WOW you're awesome! You get {int(percent_correct * 50)} health points as an award! "
         background_image_raw = pygame.image.load("images/Background_forest.jpg").convert()
         self.background_image = pygame.transform.scale(background_image_raw, screen_size)
 
